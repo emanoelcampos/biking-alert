cac2cd628a964cdac6aef4a1634b0b3973e7dabf
emanoelcampos <emanoel.cv@gmail.com>
2024-04-29 01:30:35 -0300

    feat: Add function to select training days data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from _datetime import datetime
 
 
 def get_api_data(url):
@@ -16,3 +17,13 @@ def select_weather_data(data):
     weather_dataframe = pd.concat([df_date, df_temperature, df_precipitation], axis=1)
 
     return weather_dataframe
+
+
+def select_training_days_data(weather_dataframe):
+    weather_dataframe['day_of_week'] = weather_dataframe['date'].apply(
+        lambda x: datetime.strptime(x[:10], "%Y-%m-%d").strftime('%A'))
+
+    training_days_dataframe = weather_dataframe[
+        weather_dataframe['day_of_week'].isin(['Tuesday', 'Thursday', 'Saturday'])]
+
+    return training_days_dataframe
